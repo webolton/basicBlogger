5bf82f3db53b56993a6ef96223cfa228f59a0b2a
William Bolton <william.ellet@gmail.com>
2015-09-09 13:44:16 -0400

    working on font path

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(*Rails.groups)
 module BasicBlogger3
   class Application < Rails::Application
 
+    config.serve_static_assets = true
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += /\.(?:svg|eot|woff|ttf)$/
