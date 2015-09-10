cb261d3496cf784160960761e703835ae75d7a6b
William Bolton <william.ellet@gmail.com>
2015-09-10 15:43:13 -0400

    working on font assets

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,9 @@ module BasicBlogger3
   class Application < Rails::Application
 
     # config.serve_static_assets = true
-    # config.assets.enabled = true
-    # config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    # config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
