c1e188204e5b1d99b5255a99a10dd63401a09c0c
William Bolton <william.ellet@gmail.com>
2015-09-18 18:33:32 -0400

    added I18n, German translation to subhead and logged-in notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def extract_locale_from_tld 
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 
 	def current_user
 		@users = User.all
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,12 @@ class HomeController < ApplicationController
   
   def index
   	if !session[:user_id].nil?
-      flash.notice = current_user.user_name + " is logged in."
+      flash.notice = current_user.user_name + t(:logged_in_flash)
+      # " is logged in."
     end
 
     @mainFeed = Post.all
     @feed = @mainFeed.order(id: :desc)
     @posts_desecending = Post.where(parent: nil).order(id: :desc)
-    
-
   end
-
 end
